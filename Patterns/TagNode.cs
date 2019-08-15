b13852afc21272f75ccd7b860a76e397b631c0dc
Shibu <shibu_st@outlook.com>
2019-08-15 18:14:44 +0530

    attrib

--- Patterns/TagNode.cs
+++ Patterns/TagNode.cs
@@ -6,6 +6,8 @@ namespace Patterns{
 public class TagNode{
 
     private string _tagName;
+    private string _tagAttribute = String.Empty;
+    private string _tagValue = String.Empty;
     private List<TagNode> _children = new List<TagNode>();
     public TagNode(string name)
     {
@@ -14,9 +16,17 @@ public class TagNode{
 
     public  override string ToString(){
         string result = string.Empty;
-        result += "<" + _tagName + ">";
+        result += "<" + _tagName;
+        if(_tagAttribute != String.Empty){
+            result += _tagAttribute;
+        }
+        result += ">";
+
         foreach(var item in _children){
             result += item.ToString();
+        }
+        if(_tagValue != String.Empty){
+            result += _tagValue;
         }
         result += "</" + _tagName + ">";
         return result; 
@@ -26,6 +36,16 @@ public class TagNode{
         public void Add(TagNode item)
         {
             _children.Add(item);
+        }
+
+        public void AddAttributes(string name, string value)
+        {
+            _tagAttribute += " " + name + "='" + value + "'";
+        }
+
+        public void SetValue(string value)
+        {
+            _tagValue = value;
         }
     }
 }
--- PatternsTest/TagNodeTests.cs
+++ PatternsTest/TagNodeTests.cs
@@ -28,6 +28,23 @@ namespace PatternsTest
             Assert.Equal(orders.ToString(), expectedResult);
         }
 
+        [Fact]
+        public void TestAddAttributesAndValuesToTree()
+        {
+            string expectedResult = "<orders><order><item quantity='1' number='1234'>" +
+            "DogHouse</item></order></orders>";
+            TagNode orders = new TagNode("orders");
+            TagNode order = new TagNode("order");
+            TagNode item = new TagNode("item");
+            item.AddAttributes("quantity","1");
+            item.AddAttributes("number","1234");
+            item.SetValue("DogHouse");
+
+            order.Add(item);
+            orders.Add(order);
+            Assert.Equal(orders.ToString(), expectedResult);
+        }
+
 
     }
 }
